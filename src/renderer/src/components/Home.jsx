7bfb4a13be8930ee4815f679aaeba9632787e9fc
markoka101 <markoka101@gmail.com>
2025-04-21 16:47:21 -0400

    created file for home page which displays after choosing user

---
+++ src/renderer/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserCard from './UserCard';
+import TaskCard from './TaskCard';
+import SettingsForm from './SettingsForm';
+import Stats from './Stats';
+import TaskForm from './TaskForm';
+
+const Home = () => {};
+
+export default Home;
